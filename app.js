ef2b192b77092be95555d7d96f96cfd0a183eb0e
azaanazam <168602523+azaanazam@users.noreply.github.com>
2025-08-04 05:04:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+//Question:1
+ var a =  "5";
+ var b = 2;
+ var result = a+b;
+ console.log(result); //Anser: "52"
+ //   // Concatenation because 'a' is a string
+ //  is 'a' converted to number
+
+ //Question:2
+  var a = 10;
+   var b = 5;
+   var c = ++a + b-- + --b;
+   console.log(c); //answer: 19;
+
+   //Question:3
+   var Number = ("20") * 3;
+   console.log(Number);// Answer: 60
+   // '20' is converted to a number befoprke multiplication
+
+
+   //Question:4
+   var a = 4 % 3;
+   console.log(a);// Answer:1 
+   // 4 divided by gives a remainder of 1
+
+
+   //Question:5
+   var studentName = "Hassan";
+   var score = 88;
+  alert(studentName + " got " + score + " marks in JavaScript");
+  //Answer: "Hassan" goot 88 marks in javascript
+
+
+
+  //Question:6
+  var x = 5;
+ var y = x++ + ++x - --x;
+   console.log(y); //Answer:6 
+   // x++ is post-increment, so it uses the value of x before in the expression.
+
+
+   //Question:7
+//   var 1name = "Ali";  galat
+var name1 = "Ali";   // sahii 
+
+// var var-name = "Ali"; // galat
+var var_name = "Ali";  //sahi
+
+// var student name = "Ali"; //galat
+var studentName = "Ai";  //sahii
+
+
+//Question:8
+var priceperPerPerfume = 2500;
+var totalPrice = pricePerPerfume * 4;
+alert("Total Price of 4 Perfume is " + totalPrice + "PKR");
+//Answer Total Price of 4 Perfume is 10000PKR;
+
+
+
+//Question:9
+var a = 5;
+var result = a * 2 + 10 - 3;
+console.log(result); //Eror
